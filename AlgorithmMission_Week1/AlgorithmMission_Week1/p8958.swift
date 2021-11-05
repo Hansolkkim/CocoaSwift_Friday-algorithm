1bd5db1e9960ae6fe0fc72f1f393923a81cb1f55
sol <hskim6521@naver.com>
2021-11-05 14:40:40 +0900

    p8958 코드 일부 수정

--- AlgorithmMission_Week1/AlgorithmMission_Week1/p8958.swift
+++ AlgorithmMission_Week1/AlgorithmMission_Week1/p8958.swift
@@ -7,23 +7,17 @@ func quizOX() {
     for _ in 1...howManyTimes{ //첫 줄에 입력된 값인 howManyTimes 변수의 크기만큼 반복
         let quizResult: String = readLine()!
         var countContinuos: Int = 0, totalScore: Int = 0 //countContinuos변수에 "O"가 연속된 횟수를 저장, totalScore에 총점수를 저장
-        for i in 0..<quizResult.count-1 { //마지막 index의 원소는 index+1에 값이 없으므로 0...quizResult.count-2까지만 반복
-            let myAnswer: Character = quizResult[quizResult.index(quizResult.startIndex, offsetBy: i)] //현재 위치의 정답
-            let nextAnswer: Character = quizResult[quizResult.index(quizResult.startIndex, offsetBy: i+1)] //다음 위치의 정답
+        for i in quizResult {
             
-            if myAnswer == "O"{ //현재 "O"라면
+            if i == "O"{
                 countContinuos += 1
                 totalScore += countContinuos
-                
-                if nextAnswer == "X"{
-                    countContinuos = 0
-                }
+            }else {
+                countContinuos = 0
             }
+            
         }
-        if quizResult[quizResult.index(before: quizResult.endIndex)] == "O" { //마지막 index의 원소에 따라 총점에 추가하기 위한 조건문
-            countContinuos += 1
-            totalScore += countContinuos
-        }
+        
         print(totalScore)
     }
 }
