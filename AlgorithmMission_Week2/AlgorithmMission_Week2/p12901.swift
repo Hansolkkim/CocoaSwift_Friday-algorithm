28eced50a7b4f2f0cf4225a0b8434a6fc5d2034c
sol <hskim6521@naver.com>
2021-11-12 16:18:37 +0900

    p12901 Solution 코드 작성

---
+++ AlgorithmMission_Week2/AlgorithmMission_Week2/p12901.swift
@@ -0,0 +1,24 @@
+//
+//  p12901.swift
+//  AlgorithmMission_Week2
+//
+//  Created by 김한솔 on 2021/11/12.
+//
+
+import Foundation
+
+struct p12901 {
+    
+    func dayIn2016(_ a: Int, _ b: Int) -> String {
+        let day = ["THU", "FRI", "SAT", "SUN", "MON", "TUE", "WED"]
+        let month = [0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+        var myDay = 0
+        for index in 0..<a {
+            myDay += month[index]
+        }
+        //다른 사람 풀이에서 보니, myDay = month[0..<a].reduce(0,+)로 표현해도 되던데 왠지 모르겠다.
+        myDay += b
+        
+        return day[myDay % 7]
+    }
+}
---
+++ AlgorithmMission_Week2/AlgorithmMission_Week2/p42840.swift
@@ -0,0 +1,37 @@
+//
+//  p42840.swift
+//  AlgorithmMission_Week2
+//
+//  Created by 김한솔 on 2021/11/12.
+//
+
+import Foundation
+
+struct p42840 {
+    
+    func determineWinner(_ answer: [Int]) -> [Int] {
+        
+        let Supo_1 = [1, 2, 3, 4, 5]
+        let Supo_2 = [2, 1, 2, 3, 2, 4, 2, 5]
+        let Supo_3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
+        var score = [0, 0, 0]
+        var Winner:[Int] = []
+        for questionNum in 0..<answer.count {
+            if answer[questionNum] == Supo_1[questionNum%5] {
+                score[0] += 1
+            }
+            if answer[questionNum] == Supo_2[questionNum%8] {
+                score[1] += 1
+            }
+            if answer[questionNum] == Supo_3[questionNum%10] {
+                score[2] += 1
+            }
+        }
+        for i in 0...2{
+            if score.max() == score[i] {
+                Winner.append(i+1)
+            }
+        }
+        return Winner.sorted()
+    }
+}
