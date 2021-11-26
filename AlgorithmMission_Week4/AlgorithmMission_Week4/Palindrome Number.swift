153ff8e6ef286ee2e6113e853551d9653a804788
sol <hskim6521@naver.com>
2021-11-26 12:16:14 +0900

    refactor some codes to reduce memory charge

--- AlgorithmMission_Week4/AlgorithmMission_Week4/Palindrome Number.swift
+++ AlgorithmMission_Week4/AlgorithmMission_Week4/Palindrome Number.swift
@@ -10,10 +10,6 @@ func isPalindrome(_ x: Int) -> Bool {
     let isMinus = x < 0
     if isMinus {return false}
     let inputStr = String(x)
-    var reversedStr = ""
-    for i in inputStr.reversed() {
-        reversedStr.append(i)
-    }
-    if inputStr == reversedStr {return true}
-    return false
+    let reversedStr = String(String(x).reversed())
+    return inputStr == reversedStr
 }
